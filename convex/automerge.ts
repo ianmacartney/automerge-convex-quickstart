ab8f76de49aa306d99b732a149cc361250432763
Ian Macartney <ianhmacartney@gmail.com>
2024-11-11 21:25:52 -0800

    drop log approach

--- convex/automerge.ts
+++ convex/automerge.ts
@@ -265,45 +265,6 @@ export const testToggle = mutation({
   },
 });
 
-export const version = query({
-  args: {},
-  handler: async (ctx) => {
-    return getMaxVersion(ctx);
-  },
-});
-
-async function getMaxVersion(ctx: { db: DatabaseReader }) {
-  const result = await ctx.db
-    .query("messages")
-    .withIndex("v")
-    .order("desc")
-    .first();
-  return result?.version ?? 0;
-}
-
-export const pull = query({
-  args: { after: v.number() },
-  handler: async (ctx, args) => {
-    return (
-      await ctx.db
-        .query("messages")
-        .withIndex("v", (q) => q.gt("version", args.after))
-        .order("desc")
-        .take(100)
-    ).reverse();
-  },
-});
-
-export const send = mutation({
-  args: schema.tables.messages.validator.fields.message,
-  handler: async (ctx, args) => {
-    // TODO: add sequence number
-    const version = (await getMaxVersion(ctx)) + 1;
-    await ctx.db.insert("messages", { message: args, version });
-    return version;
-  },
-});
-
 const peerId = process.env.CONVEX_CLOUD_URL as PeerId;
 
 export const create = mutation({
